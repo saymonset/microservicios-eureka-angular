0abaeb5262b6eede99cb5a7f551a28ea8f242789
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2023-01-09 08:32:25 -0400

    first commit

---
+++ microservicios-usuarios/src/main/java/com/formacionbdi/microservicios/app/usuarios/services/AlumnoServiceImpl.java
@@ -0,0 +1,22 @@
+package com.formacionbdi.microservicios.app.usuarios.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.formacionbdi.microservicios.app.usuarios.models.repository.AlumnoRepository;
+import com.formacionbdi.microservicios.commons.alumnos.models.entity.Alumno;
+import com.formacionbdi.microservicios.commons.services.CommonServiceImpl;
+
+@Service
+public class AlumnoServiceImpl extends CommonServiceImpl<Alumno, AlumnoRepository> implements AlumnoService {
+
+	@Override
+	@Transactional(readOnly = true)
+	public List<Alumno> findByNombreOrApellido(String term) {
+		return repository.findByNombreOrApellido(term);
+	}
+	
+
+}
