523206b9f992ff7ca23eabead60e8489b4d27347
Simon Alberto Rodriguez Pacheco <saymon_set@hotmail.com>
2023-01-19 08:48:40 -0400

    Angular y comunicacion con microservicos

--- microservicios-examenes/src/main/java/com/formacionbdi/microservicios/app/examenes/models/repository/AsignaturaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.formacionbdi.microservicios.app.examenes.models.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.formacionbdi.microservicios.commons.examenes.models.entity.Asignatura;
-
-public interface AsignaturaRepository extends CrudRepository<Asignatura, Long> {
-
-}
